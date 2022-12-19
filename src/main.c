d319dc33682e7e4d59ccfa847067b6cab83509f2
marwan0620@gmail.com <marwan0620@gmail.com>
2022-12-19 16:46:13 +0100

    MINISHELL 🤙 FINITOoOoOoOOOOOO 🤙

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: mayoub <mayoub@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/13 15:04:03 by flemaitr          #+#    #+#             */
-/*   Updated: 2022/12/19 13:33:19 by mayoub           ###   ########.fr       */
+/*   Updated: 2022/12/19 16:44:48 by mayoub           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,21 +29,6 @@ int	main(int argc, char **argv, char **envv)
 
 /*
 
-* redirection:
-	* expend le name du fichier de output
-	? input  : `` echo > $[VARIABLE QUI N'EXISTE PAS] ``
-	? output : `` [minishell]: $[VARIABLE QUI N'EXISTE PAS]: ambiguous redirect ``
-	? ===> A FIX
-
-* heredoc:
-	* mettre plusieurs heredoc dans la meme pipe...
-
-* redirection 'in >':
-	* '> [RANDOM]' ===> print :
-		![minishell]: syntax error near unexpected token `|'
-		* alors qu'il ne devrait pas, mais fonctionne
-
-? norme et nettoyage
-! C'EST FAIS
+! FINITOOOOOOOOOO !
 
 */
